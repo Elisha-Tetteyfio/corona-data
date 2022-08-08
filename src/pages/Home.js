0454b223ae1d8bbc7d186528d23e8fa1713d7897
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-08-08 20:00:58 +0000

    Add paths to individual details page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux/es/exports';
+import { NavLink } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import CountryCard from '../components/CountryCard';
 
@@ -10,11 +11,13 @@ const Home = () => {
       <h1>Corona Data</h1>
       <div>
         {countries.map((country) => (
-          <CountryCard
-            name={country.name}
-            latest_data={country.latest_data}
-            key={uuidv4()}
-          />
+          <NavLink to={`/details/${country.name}`} exact key={uuidv4()}>
+            <CountryCard
+              name={country.name}
+              latest_data={country.latest_data}
+              key={uuidv4()}
+            />
+          </NavLink>
         ))}
       </div>
     </div>
