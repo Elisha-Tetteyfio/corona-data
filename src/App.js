1ed3aa70fe39f36309a26ec3c7eb05502bd11207
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-08-08 13:02:40 +0000

    Add linters configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Corona Data</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Corona Data</h1>
+  </div>
+);
 
 export default App;
